51ea27b38b6f4b4702e4a61950dd6418a6c2d953
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-17 11:07:30 -0800

    Proof of life

--- server.js
+++ server.js
@@ -2,14 +2,33 @@
 
 
 // call in all requirements for project
-require ('dotenv').config();
+const PORT = process.env.PORT || 3000
+require('dotenv').config();
 const express = require('express');
 const pg = require('pg');
-
 const app = express();
+const ejs = require('ejs');
 
-const PORT = process.env.PORT
+
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on
+client.on('error', err => console.error(err));
+
+// set view engine
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.render('./pages/index');
+});
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`up on port ${PORT}`));
+
