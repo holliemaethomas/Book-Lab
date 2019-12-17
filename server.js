87075683bfa3a656bc60c5b35795ad92caf36433
Dayne Daylong <dalexad@gmail.com>
2019-12-17 13:58:21 -0800

    frgwver

--- server.js
+++ server.js
@@ -2,19 +2,26 @@
 
 
 // call in all requirements for project
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3099
 require('dotenv').config();
 const express = require('express');
 const pg = require('pg');
 const app = express();
-const ejs = require('ejs');
 const superagent = require('superagent');
 
-
+// const ejs = require('ejs');
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
+
+///// constructor
+// function ObjectifyBook (title, author) {
+//   this.title = title,
+//   this.author = author
+//   this.formatted_query = formatted_address,
+//   this.search_query = search_query
+// }
 
 // set view engine
 app.set('view engine', 'ejs');
@@ -26,24 +33,24 @@ app.get('/', (req, res) => {
   res.render('./pages/index');
 });
 
-
 app.post('/searches', (req, res) => {
+
   superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data => {
 
     const books = data.body.items.map(book => ({name: book.volumeInfo.title}));
 
     console.log(books);
 
-    res.render('book-results', {
-      books: books
-    });
+    res.render('./pages/searches');
+
+    // res.render('book-results', {
+    //   books: books
+    // });
   });
-
-
 });
 
 
 
+app.listen(PORT, () => console.log(`Port ${PORT} for the win!`));
 
-app.listen(PORT, () => console.log(`up on port ${PORT}`));
 
