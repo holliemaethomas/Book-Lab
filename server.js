c61519b124a5d40ccefb4030ddbeb2843aade441
holliemaethomas <holliemaethomas85@gmail.com>
2019-12-18 12:32:11 -0800

    fixed error catch

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const books = [];
 // function to handle errors
 function errors(error, response) {
   console.error(error);
-  response.render('./views/pages/error');
+  response.render('./pages/error');
 }
 console.log(errors);
 
@@ -30,9 +30,8 @@ app.get('/', (req, res) => {
 
 /////route creation?
 // credit for this functionality is from class demo
-app.post('/show', (req, res) => {
+app.post('show', (req, res) => {
   superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.searchType}+in${req.body.searchType}:${req.body.query}`).then(data => {
-    console.log(data);
     for (let i = 0; i < 10; i++) {
       books.push(data.body.items[i])
     }
@@ -41,10 +40,13 @@ app.post('/show', (req, res) => {
     const returns = books.map(book => {
       return new BookObject(book.volumeInfo.title, book.volumeInfo.authors, book.volumeInfo.description)
     })
-    res.render('views/pages/show', { returns: returns });
+    res.render('./pages/show', { returns: returns });
   })
-    .catch(errors)
+    .catch(err => {
+      errors(err, res)
+    });
 });
+// yosh helped me write the error function
 
 function BookObject(books) {
   this.title = books.volumeInfo.title
